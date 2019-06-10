4b6f9669dbca9ff7c26654906938d93e0b0e8de3
Tomofumi Inoue <funaox@gmail.com>
2019-06-10 04:54:10 +0000

    都道府県別の総人口推移グラフを実装

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '都道府県別の総人口推移グラフ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -27,7 +27,12 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    {
+      src: '~/plugins/highcharts.js',
+      ssr: false
+    }
+  ],
   /*
    ** Nuxt.js modules
    */
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,117 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        population-transition
-      </h1>
-      <h2 class="subtitle">
-        都道府県別の総人口推移グラフを表示するSPA
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
+    <h1>都道府県別の総人口推移グラフ</h1>
+    <form>
+      <label class="pref-label">都道府県</label>
+      <b-form-checkbox-group v-model="checkedPrefCodes" class="pref-checkboxes">
+        <b-form-checkbox
+          v-for="prefecture in prefectures"
+          :key="prefecture.prefCode"
+          :value="prefecture.prefCode"
+          class="pref-checkbox"
         >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
-    </div>
+          {{ prefecture.prefName }}
+        </b-form-checkbox>
+      </b-form-checkbox-group>
+    </form>
+    <highcharts :options="chartOptions"></highcharts>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { Chart } from 'highcharts-vue'
+import Highcharts from 'highcharts'
 
+const API_KEY = '6ppvd355tMqUlBs0saBNhLhmLRKgHn7Wbyp7q7Zc'
 export default {
   components: {
-    Logo
+    highcharts: Chart
+  },
+  data() {
+    return {
+      prefectures: [],
+      checkedPrefCodes: null,
+      chartOptions: {
+        title: {
+          text: null
+        },
+        tooltip: {
+          formatter: function() {
+            return (
+              '<b>' +
+              this.x +
+              '</b><br/>' +
+              this.series.name +
+              ': ' +
+              Highcharts.numberFormat(this.y, 0, ',', ',') +
+              '人'
+            )
+          }
+        },
+        xAxis: {
+          title: {
+            text: '年度'
+          }
+        },
+        yAxis: {
+          title: {
+            text: '人口数'
+          },
+          labels: {
+            formatter: function() {
+              return Highcharts.numberFormat(this.value, 0, ',', ',')
+            }
+          }
+        },
+
+        plotOptions: {
+          series: {
+            pointStart: 1960,
+            pointInterval: 5
+          }
+        },
+        series: []
+      }
+    }
+  },
+  watch: {
+    checkedPrefCodes: async function(checkedPrefCodes) {
+      if (checkedPrefCodes == null) return
+      const series = []
+      for (const checkedPrefCode of checkedPrefCodes) {
+        const data = await this.$axios
+          .get(
+            'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=' +
+              checkedPrefCode,
+            {
+              headers: {
+                'Content-Type': 'application/json',
+                'X-API-KEY': API_KEY
+              }
+            }
+          )
+          .then(response => response.data.result.data[0].data)
+        series.push({
+          name: this.getPrefNameByCode(checkedPrefCode),
+          data: data.map(d => d.value)
+        })
+      }
+      this.chartOptions.series = series
+    }
+  },
+  async mounted() {
+    this.prefectures = await this.$axios
+      .get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: { 'Content-Type': 'application/json', 'X-API-KEY': API_KEY }
+      })
+      .then(response => response.data.result)
+  },
+  methods: {
+    getPrefNameByCode(code) {
+      return this.prefectures.filter(
+        prefecture => prefecture.prefCode === code
+      )[0].prefName
+    }
   }
 }
 </script>
@@ -41,27 +124,26 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+form {
+  padding-left: 196px;
+}
+
+.pref-label {
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  text-align: left;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.pref-checkboxes {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 640px;
 }
 
-.links {
-  padding-top: 15px;
+.custom-control-label {
+  width: 96px;
+  text-align: left;
 }
 </style>
